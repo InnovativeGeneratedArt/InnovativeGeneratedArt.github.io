82ca2dc2c8ab74b12f02cd4693d21884faf8dffc
InnovativeGeneratedArt <1novative.generated.art@gmail.com>
2023-01-09 07:32:35 +0200

    Add js for 3D gallery

--- js/index.js
+++ js/index.js
@@ -82,12 +82,3 @@ const animate = function () {
 };
 
 animate();
-AFRAME.registerComponent("look-at", {
-  schema: { type: "selector" },
-
-  init: function () {},
-
-  tick: function () {
-    this.el.object3D.lookAt(this.data.object3D.position);
-  },
-});
