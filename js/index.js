98ce7651afe4f98c24648986439c91a9ebb3433a
InnovativeGeneratedArt <1novative.generated.art@gmail.com>
2023-01-09 07:29:10 +0200

    Update JS

--- js/index.js
+++ js/index.js
@@ -82,3 +82,12 @@ const animate = function () {
 };
 
 animate();
+AFRAME.registerComponent("look-at", {
+  schema: { type: "selector" },
+
+  init: function () {},
+
+  tick: function () {
+    this.el.object3D.lookAt(this.data.object3D.position);
+  },
+});
