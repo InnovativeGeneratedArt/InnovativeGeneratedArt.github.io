a5765728eb43433b2fcd604e7291f67e71aab3b2
InnovativeGeneratedArt <1novative.generated.art@gmail.com>
2023-01-09 07:59:53 +0200

    Update index2.js

--- js/index2.js
+++ js/index2.js
@@ -1,12 +1,12 @@
 AFRAME.registerComponent("look-at", {
   schema: { type: "selector" },
 
-  init: function () {},
-
-  tick: function () {
+  init: function () {
     var cameraEl = document.getElementById("camera");
     cameraEl.setAttribute("look-controls", { enabled: false });
     this.el.object3D.lookAt(this.data.object3D.position);
     cameraEl.setAttribute("look-controls", { enabled: true });
   },
+
+  tick: function () {},
 });
